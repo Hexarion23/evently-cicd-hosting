8a2ab9e6528946fecf9d9bfe8f2de0973ef087c5
Hexarion23 <kakefortytwo@gmail.com>
2026-02-19 12:09:01 +0800

    deleting

--- src/models/supabaseClient.js
+++
@@ -1,9 +0,0 @@
-const { createClient } = require("@supabase/supabase-js");
-
-const supabaseUrl = process.env.SUPABASE_URL || 'https://fyfyvadrabgptmbiuvcl.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5Znl2YWRyYWJncHRtYml1dmNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMyMDE5OTYsImV4cCI6MjA3ODc3Nzk5Nn0.kcqDjL3ZaZVOlVU7sDlcl6x5MxrWPlZ_681hzYbbSbQ';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-module.exports = { supabase };
-  
--- src/routers/attendance.router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const attendanceController = require("../controllers/attendance.controller");
-router.post("/scan", attendanceController.scanAttendance);
-
-module.exports = router;
